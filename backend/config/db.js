38d76394bf46fa315334f7d36f0a80ba92510ec4
PapeMoussa03 <gassamapapemoussa@gmail.com>
2026-05-07 22:14:43 +0000

    fix: utilisation de Clever Cloud MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
   connectTimeout: 30000,
